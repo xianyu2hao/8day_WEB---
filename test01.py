e5cd09b011700923a37a67ab0454ec2a899a27d8
xianyu2hao <3148541956@qq.com>
2021-07-28 10:56:20 +0800

    Update test01.py

--- test01.py
+++ test01.py
@@ -2,3 +2,6 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 driver.get("https://www.baidu.com/")
 driver.quit()
+
+
+# 安装好，python后，按照该博主装没问题：https://blog.csdn.net/orange_xiang/article/details/82924296?utm_medium=distribute.pc_relevant_t0.none-task-blog-2%7Edefault%7EBlogCommendFromMachineLearnPai2%7Edefault-1.control&depth_1-utm_source=distribute.pc_relevant_t0.none-task-blog-2%7Edefault%7EBlogCommendFromMachineLearnPai2%7Edefault-1.control
